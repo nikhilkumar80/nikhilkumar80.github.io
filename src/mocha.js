b13dbd6e61282dd70c7b340498ea8c902bb0d30f
nikhilk <nikhilk@yahoo-inc.com>
2017-03-04 10:08:41 -0800

    Adding sinon-chai

---
+++ src/mocha.js
@@ -0,0 +1,10 @@
+"use strict";
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+chai.should();
+chai.use(sinonChai);
+
+module.exports = {
+    test: 1
+}
